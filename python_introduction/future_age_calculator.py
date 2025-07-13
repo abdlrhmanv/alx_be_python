b800683d9657526306c0da634388808bb5a080d9
Abdlrhman <abdlrhmanv@icloud.com>
2025-07-14 00:19:35 +0300

    4. User Input Age Calculator

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,4 +1,3 @@
-prompt = "How old are you?"
-current_age = int(input(prompt))
+current_age = int(input("How old are you?"))
 years2025 = current_age + 27
 print("In 2025, you will be", years2025, "years old.")
